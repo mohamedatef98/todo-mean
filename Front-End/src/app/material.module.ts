162f46c411f5480835bf29fc1d2739a5088288d4
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-06-04 01:11:24 +0200

    Layout components

---
+++ Front-End/src/app/material.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule} from "@angular/material";
+
+
+@NgModule({
+  exports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
+})
+export class MaterialModule{
+
+}
