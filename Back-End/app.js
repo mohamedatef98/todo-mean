cb0749da770055c1d6e74be4c47d707f9d9a2896
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-06-06 10:37:21 +0200

    CORS middleware

--- Back-End/app.js
+++ Back-End/app.js
@@ -12,6 +12,16 @@ const todosRouter = require('./routes/todos');
 
 const app = express();
 
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    methods: 'PUT, POST, GET, DELETE, OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
