083863ce157b4ba3d84fefcfe2f11724a9cc4b29
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-06-06 20:28:30 +0200

    Logout message

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
     // Catch all other routes and return the index file
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/index.html'));
+        res.sendFile(path.join(__dirname, './dist/index.html'));
     });
 
     app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {Router} from "@angular/router";
 import {TokenService} from "../../../services/token.service";
+import { NotificaionsService } from 'src/app/services/notifications.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,7 @@ import {TokenService} from "../../../services/token.service";
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent{
-  constructor(private tokenService: TokenService, private router: Router){}
+  constructor(private tokenService: TokenService, private router: Router, private notificationsService: NotificaionsService){}
 
   noToken(){
     return !this.tokenService.getToken();
@@ -17,5 +18,6 @@ export class NavbarComponent{
   logout(){
     this.tokenService.removeToken();
     this.router.navigate(['/'])
+    this.notificationsService.pushSuccess('Logout!, See you soon!')
   }
 }
