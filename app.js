75ada32728292d965c3fca2e92f9ee4ef65461b5
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-06-06 18:50:20 +0200

    port typo fix

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
         res.sendFile('./dist/index.html');
     });
 
-    app.listen(process.env.PORT, () => console.log(`Example app listening on port ${port}!`))
+    app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
 })();
 
 module.exports = app;
